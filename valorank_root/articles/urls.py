83850a75c7a3aa3840351bb8e55b69d400b3d237
danisimore <danisimore@yandex.ru>
2022-12-06 19:36:45 +0300

    added 'article_detail' page

--- valorank_root/articles/urls.py
+++ valorank_root/articles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ArticleListView.as_view(), name='articles')
+    path('', views.ArticlesListView.as_view(), name='articles'),
+    path('/detail/<int:pk>', views.ArticleDetailView.as_view()),
 ]
--- valorank_root/articles/views.py
+++ valorank_root/articles/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 
 from .models import Article
 
-class ArticleListView(ListView):
+class ArticlesListView(ListView):
     template_name = 'articles.html'
     model = Article
 
@@ -11,5 +11,18 @@ class ArticleListView(ListView):
         context = super().get_context_data(**kwargs)
         context['updates'] = Article.objects.filter(category=1)
         context['information_and_articles'] = Article.objects.all().exclude(category=1)
+
+        return context
+
+class ArticleDetailView(TemplateView):
+    template_name = 'article_detail.html'
+
+    def get_context_data(self, **kwargs):
+        selected_article_pk = kwargs['pk']
+
+        context = super().get_context_data(**kwargs)
+        context['article'] = Article.objects.filter(pk=selected_article_pk)
+        context['other_articles'] = Article.objects.all().exclude(pk=selected_article_pk).order_by('-pk')[:3]
+
 
         return context
